a205a20bb7ab7866d605872ceb9017005a1d7319
Carlos Martín Nieto <cmn@dwim.me>
2013-10-18 22:25:08 +0200

    WIP commit_create

--- test/repo_test.erl
+++ test/repo_test.erl
@@ -6,7 +6,8 @@
 repo_test_() ->
     {foreach, fun start/0, fun stop/1, [fun bare_test/1, fun odb_write_test/1,
 					fun ref_test/1, fun index_add_test/1,
-					fun ref_iter_test/1, fun revparse_test/1]}.
+					fun ref_iter_test/1, fun revparse_test/1,
+					fun commit_create_test/1]}.
 
 start() ->
     {A, B, C} = now(),
@@ -81,6 +82,16 @@ ref_iter_test(Repo) ->
      ?_assertEqual(Res0, {error, iterover}),
      ?_assertEqual(Ref2#geef_reference.name, <<"refs/heads/branch">>),
      ?_assertEqual(Res1, {error, iterover})].
+
+commit_create_test(Repo) ->
+    odb_write_test(Repo),
+    index_add_test(Repo),
+    Tree = geef_oid:parse("5a20bbbf65ea75ad4d9f995d179156824ccca3a1"),
+    Sig = #geef_signature{name= <<"foo">>, email= <<"bar">>, time={{1381,949139,0}, 120}},
+    Message = <<"Commit message">>,
+    Resp = geef_commit:create(Repo, Sig, Message, Tree, []),
+    ?debugMsg(Resp),
+    [?_assertMatch({ok, _}, Resp)].
 
 rm_r(Path) ->
     case filelib:is_dir(Path) of
